c5ec7a1ba0aed32e133b04cb1ec34799576c2e5b
DeekshaSingh011 <84437447+DeekshaSingh011@users.noreply.github.com>
2025-08-19 18:07:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import React from "react";
 import Banner from "./components/banner";
 import Exhibit from "./components/exhibit";
 import IPAddress from "./components/IPAddress";
-import Latency from "./components/Latency"; // <-- added
+import Latency from "./components/Latency";
 
 function App() {
   return (
     <div className="App" style={{ fontFamily: 'sans-serif', backgroundColor: '#F8FAFC', minHeight: '100vh' }}>
+      {/* Top Banner */}
       <Banner title="Welcome to Sextant!" />
+
       <main>
+        {/* Example Tic-Tac-Toe Exhibit (already present) */}
         <Exhibit heading="Tic-Tac-Toe Game">
           <p>Tic-tac-toe</p>
         </Exhibit>
 
+        {/* Public IPs (IPv4 + IPv6 in separate Exhibits) */}
         <Exhibit heading="Public IPv4 Address">
           <IPAddress version="v4" />
         </Exhibit>
@@ -22,10 +26,12 @@ function App() {
           <IPAddress version="v6" />
         </Exhibit>
 
+        {/* Packet Latency Exhibit */}
         <Exhibit heading="Packet Latency">
-          <Latency /> {/* <-- added */}
+          <Latency />
         </Exhibit>
 
+        {/* About Exhibit */}
         <Exhibit heading="About">
           <p>This site demonstrates component composition using React.</p>
         </Exhibit>
